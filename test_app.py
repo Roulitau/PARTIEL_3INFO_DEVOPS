0366feecdf68d84312d05798d54bba8ccb606c99
Roulitau <jul1092.jul1092@gmail.com>
2025-02-26 12:06:47 +0100

    update linter

--- test_app.py
+++ test_app.py
@@ -9,18 +9,15 @@ class TestAddFunction(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
-
     def test_multiply(self):
         self.assertEqual(multiply(2, 3), 6)
         self.assertEqual(multiply(-1, 5), -5)
         self.assertEqual(multiply(0, 10), 0)
 
-
     def test_divide(self):
         self.assertEqual(divide(10, 2), 5)
         self.assertEqual(divide(5, 0), None)
         self.assertEqual(divide(0, 5), 0)
-
 
     def test_greet(self):
         self.assertEqual(greet("Alice"), "Hello, Alice")
